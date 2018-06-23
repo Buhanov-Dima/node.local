c1880954e8b4fe2f41b22137edf00062581a834b
Buhanov-Dima <35217770+Buhanov-Dima@users.noreply.github.com>
2018-06-23 21:17:28 +0500

    add technology, footer(not end)

--- index.php
+++ index.php
@@ -120,8 +120,71 @@
 		</div>
 	</div>
 </section>
+<section id="block-4">
+	<div class="node-b4">
+		<div class="logo-node-b4">
+			<img src="/img/icons/logo-wh.png" alt=""><p><b>node</b>power</p>
+		</div>
+		<img src="/img/icons/world-wh.png" alt="">
+	</div>
+	<div class="container">
+		<div class="row">
+			<div class="col">
+				<div class="wrap-b4">
+					<h1 class="standart-h1">Technology</h1>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="btn-center">
+		<a href="#" class="btn-down-1 white-txt arrow-right"><span></span>Read more about node technology</a>
+	</div>
+</section>
 <footer>
-	
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6">
+				<div class="contacts">
+					<h3>Contact us</h3>
+					<p>
+						<a href="#">info@nodepower.com</a><br>
+						<a href="#">+ 371 661 638 56</a>
+					</p>
+					<p>Riga, Matisa iela 61 - 28, LV - 1009</p>
+				</div>
+			</div>
+			<div class="col-md-6">
+				<div class="subscribe">
+					<div class="">
+						<form class="footer-form" action="">
+							<input type="text" class="e-mail" placeholder="E-MAIL">
+							<button class="sub-btn">subscribe</button>
+						</form>
+					</div>
+					<div class="footer-menu">
+						<a href="#">Eon</a>
+						<a href="#">Technology</a>
+						<a href="#">Company</a>
+						<a href="#">Careers</a>
+					</div>
+					<div class="social">
+						<a href="#"><img src="/img/icons/fb.png" alt="facebook"></a>
+						<a href="#"><img src="/img/icons/twit.png" alt="twitter"></a>
+						<a href="#"><img src="/img/icons/inst.png" alt="instagram"></a>
+						<a href="#"><img src="/img/icons/mail.png" alt="email"></a>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col">
+				<div class="down-line">
+					<p>Copyright © 2018 Node SIA.</p>
+					<p>All rights reserved. </p>
+				</div>
+			</div>
+		</div>
+	</div>
 </footer>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
