10d0b6514e67b705e88d868de65130818a04f46a
Buhanov-Dima <35217770+Buhanov-Dima@users.noreply.github.com>
2018-06-25 21:52:04 +0500

    eon (end)

---
+++ eon.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+	<title>Node</title>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=Edge"/>
+  <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+  <link rel="shortcut icon" href="https://hello.node.io/wp-content/themes/node/assets/img/favicon.ico" type="image/x-icon">
+	<link rel="icon" href="https://hello.node.io/wp-content/themes/node/assets/img/favicon.ico" type="image/x-icon">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600,700,800,900" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Roboto+Mono:300,400,500,700" rel="stylesheet">
+	<link rel="stylesheet" href="/css/style.css">
+</head>
+<body>
+<section id="eon-b1">
+	<header>
+		<nav class="navbar navbar-expand-lg bg-header">
+		<div class="fx-cnt-between">
+		  <a class="navbar-brand logo white-txt" href="#">
+		  	<img src="/img/icons/logo-wh.png" alt=""><p><b>node</b>power</p>
+		  </a>
+		  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		    <span class="navbar-toggler-icon"></span>
+		  </button>
+		
+		  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+		    <ul class="navbar-nav white-txt mx-auto">
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">eon</span></a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">technology</a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">company</a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">contacts</a>
+		      </li>
+		    </ul>
+		  </div>
+		  <div class="lang">
+		  	<ul>
+		  	  <li class="nav-item dropdown">
+		        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+		          <img src="/img/icons/world-wh.png" alt="">
+		        </a>
+		        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+		          <a class="dropdown-item" href="#">Action</a>
+		          <a class="dropdown-item" href="#">Another action</a>
+		          <div class="dropdown-divider"></div>
+		          <a class="dropdown-item" href="#">Something else here</a>
+		        </div>
+		      </li>
+		    </ul>
+		   </div>
+		  </div>
+		</nav>
+	</header>
+	<div class="container">
+		<div class="row justify-content-center">
+			<div class="col-xl-12">
+				<h1 class="standart-h1">Eon</h1>
+					<div class="wrap-eon">
+					<p>The wireless power pod by Nodepower</p>
+					<div class="main-img-eon">
+						<a href="#" class="btn-eon active">Promo video</a>
+						<div class="eon-main-center">
+							<img src="/img/main/eon-black.png" alt="">
+						</div>
+						<a href="#" class="btn-eon">Prototype video</a>
+					</div>
+				</div>
+				<div class="eon-txt">
+					<p>Wireless charging station for desktop placement</p>
+					<span>It has small dimensions and is capable of transmitting energy within a radius<br>of up to 5 meters, being at an altitude of up to 200 mmfrom the level of the surface<br> on which it is located. Wireless Power Pod provides charging of several devices<br> simultaneously and is equipped with automatic power control.</span>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<section id="eon-b2" class="mg-t-6">
+	<div class="container">
+		<div class="row">
+			<div class="col"><h1 class="standart-h1-000 mg-b-2">Technical specs</h1></div>
+		</div>
+		<div class="row">
+			<div class="col-xl-6">
+				<div class="wrap-eon-b1">
+					<img src="/img/main/eon-img-1.svg" alt="">
+				</div>
+				<div class="wrap-eon-txt">
+					<p>Diameter 2,76 inches (70,1 mm)</p>
+				</div>
+			</div>
+			<div class="col-xl-6">
+				<div class="wrap-eon-b2">
+					<img src="/img/main/eon-img-2.svg" alt="">
+				</div>
+				<div class="wrap-eon-txt">
+					<p>Diameter 2,76 inches (70,1 mm)</p>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col">
+				<div class="wrap-articles">
+					<div class="article">
+						<p>Dimensions:</p>
+						<span>Diameter 4.72 inches (120 mm)<br>High 2.75 inches (70 mm)</span>
+					</div>
+					<div class="article">
+						<p>Wireless Power Receiver:</p>
+						<span>Lightning, USB Type-C,<br>micro-USB</span>
+					</div>
+					<div class="article">
+						<p>Power:</p>
+						<span>Input Voltage - 220V<br>Output Voltage - 5V</span>
+					</div>
+					<div class="article">
+						<p>color:</p>
+						<span>Black / White</span>
+					</div>
+					<div class="article">
+						<p>Operating temperature:</p>
+						<span>32° to 95° F (0° to 35° C)</span>
+					</div>
+					<div class="article">
+						<p>Relative Humidity:</p>
+						<span>5% to 95% non condensing</span>
+					</div>
+					<div class="article">
+						<p>Compatibility:</p>
+						<img src="/img/icons/pasted.png" alt="">
+						<span>Qi wireless power standard</span>
+					</div>
+					<div class="article">
+						<p>In The Box</p>
+						<span>Eon Wireless Power Pod<br>Wireless Power Receiver<br>Power Cord <br>User Guide to wireless charging</span>
+					</div>
+					<div class="article">
+						<p>Operating Altitude:</p>
+						<span>Tested up to 10,000 feet<br>(3000 m)</span>
+					</div>
+					<div class="article article-last">
+						<p>Eon and the Environment</p>
+						<span>Nodepower is committed to reducing its environmental impact through out<br> Eon's entire life cycle, and takes a holistic approach to material<br> management and waste minimization. Eon is designed with the following<br> features in order to adhere to this commitment</span>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<?php include('footer.php'); ?>
