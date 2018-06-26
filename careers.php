04707d354d621f0cd0a88dad0e9d428e9ec9aff7
Buhanov-Dima <35217770+Buhanov-Dima@users.noreply.github.com>
2018-06-26 19:44:27 +0500

    company end

--- careers.php
+++ careers.php
@@ -1,4 +1,4 @@
-<?php include('header.php') ?>
+<?php include('header.php'); ?>
 
 		<div class="container">
 			<div class="row">
@@ -7,7 +7,7 @@
 						<h1 class="standart-h1-000">JOIN OUR WIRELESS TEAM</h1>
 						<p class="careers-txt mg-t-1">Do you want to become a part of a revolutionizing company?</p>
 						<p class="careers-txt mg-t-8">Node is seeking talented, creative and passionate people who are ready to undertake<br> challenges and make the wireless future happen! Our team of open-minded<br>professionals welcomes all the candidates who are ready to take a step forward and<br>push the boundaries of wireless technology.</p>
-						<i></i>
+						<i class="line-purple"></i>
 						<p class="careers-txt mg-t-1">Let’s power-up the world together!</p>
 						<p class="car-txt-1 mg-t-10">Open positions</p>
 						<ul class="careers-ul">
@@ -33,4 +33,4 @@
 			</div>
 		</div>
 
-<?php include('footer.php') ?>
+<?php include('footer.php'); ?>
