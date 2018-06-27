c8b801ae16503ec8aaf80cfa0e309ad0a9b95144
Buhanov-Dima <35217770+Buhanov-Dima@users.noreply.github.com>
2018-06-27 19:53:22 +0500

    technology (not end)

--- js/main.js
+++ js/main.js
@@ -1,10 +1,19 @@
 $(document).ready(function(){
 
-$(".careers-ul li").click(function(e) {
-  e.preventDefault();
-  $(".careers-ul li").removeClass('active');
-  $(this).addClass('active');
-})
+	$(".careers-ul li").click(function(e) {
+	  e.preventDefault();
+	  $(".careers-ul li").removeClass('active');
+	  $(this).addClass('active');
+	});
+
+	$('.tab-box a').click(function(e) {
+        e.preventDefault();
+        $('.tab-box .active').removeClass('active');
+        $(this).addClass('active');
+        var tab = $(this).attr('href');
+        $('.tech-b3').not(tab).css({'display':'none'});
+        $(tab).fadeIn(400);
+    })
 
 
 });
---
+++ technology.php
@@ -0,0 +1,176 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+	<title>Node</title>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=Edge"/>
+  <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+  <link rel="shortcut icon" href="https://hello.node.io/wp-content/themes/node/assets/img/favicon.ico" type="image/x-icon">
+	<link rel="icon" href="https://hello.node.io/wp-content/themes/node/assets/img/favicon.ico" type="image/x-icon">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600,700,800,900" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Roboto+Mono:300,400,500,700" rel="stylesheet">
+	<link rel="stylesheet" href="/css/style.css">
+</head>
+<body>
+<section id="tech">
+<div class="bg-tech"></div>
+	<header>
+		<nav class="navbar navbar-expand-lg bg-header">
+		<div class="fx-cnt-between">
+		  <a class="navbar-brand logo white-txt" href="#">
+		  	<img src="/img/icons/logo-wh.png" alt=""><p><b>node</b>power</p>
+		  </a>
+		  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		    <span class="navbar-toggler-icon"></span>
+		  </button>
+		
+		  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+		    <ul class="navbar-nav white-txt mx-auto">
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">eon</span></a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">technology</a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">company</a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">contacts</a>
+		      </li>
+		    </ul>
+		  </div>
+		  <div class="lang">
+		  	<ul>
+		  	  <li class="nav-item dropdown">
+		        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+		          <img src="/img/icons/world-wh.png" alt="">
+		        </a>
+		        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+		          <a class="dropdown-item" href="#">Action</a>
+		          <a class="dropdown-item" href="#">Another action</a>
+		          <div class="dropdown-divider"></div>
+		          <a class="dropdown-item" href="#">Something else here</a>
+		        </div>
+		      </li>
+		    </ul>
+		   </div>
+		  </div>
+		</nav>
+	</header>
+	<div class="container">
+		<div class="row justify-content-center">
+			<div class="col-xl-12">
+				<h1 class="standart-h1">Nodepower Technology</h1>
+					<div class="wrap-tech">
+					<i class="line-purple"></i>
+					<p>Nodepower's wireless transmission technology is based<br>on the unique method of forming an electromagnetic wave with<br>special spatial characteristics by creating an electromagnetic<br>field of a complex shape in the proximate area.<br>Within the formation of such a wave, the main principle lies in<br><span>the interference of electromagnetic waves.</span></p>
+					<img src="/img/icons/wifi.png" alt="">
+					</div>
+			</div>
+		</div>
+	</div>
+</section>
+<section>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="how_it">
+					<h1 class="standart-h1-000">how it works</h1>
+					<div class="wrap-circle">
+						<div class="how_circle"><i></i></div>
+						<div class="how_circle"><i></i></div>
+					</div>
+					<p class="text-center mg-t-10 mg-b-5">Drones and quadcopters can be continuously<br> recharged from wireless charging stations<br> on theroute. There is an opportunity</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<section id="tech-b3-vh">
+	<div class="container-fluid pdg-0">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="wrap-tech-b3">
+					<div class="tech-b3 bg-1" id="tab-1">
+						<h5>Consumer Electronics</h5>
+						<p>Built on Node technology power pod Eon not only<br> keeps smartphones, smartwatches, fitness trackers<br> continuously charged without wires or plugs, they<br> also gives you more freedom use your gadgets</p>
+					</div>
+					<div class="tech-b3 bg-2" id="tab-2">
+						<h5>IOT</h5>
+						<p>Node real wireless power delivers<br> continuous, safe energy without us having<br> to think about it. Just wait for a couple years<br> and you will see smart homes and smart<br> cities made with Node technology</p>
+					</div>
+					<div class="tech-b3 bg-3" id="tab-3">
+						<h5>Automotive</h5>
+						<p>You can leave you electric car on the parking with<br> Node power station and don't think about cables<br> and converters. Also we can built Node<br> transmitter right into your vehicles, every time<br> you drive, your Node-empowered devices, from<br> smartphones</p>
+					</div>
+					<div class="tech-b3 bg-4" id="tab-4">
+						<h5>Retail</h5>
+						<p>Imagine entire store with digital price tags,<br> without wires or batteries, which allows to<br> remotely add promotions in an instant. Mobile<br> checkout stations with always-charged devices,<br> accurate pricing, and data.</p>
+					</div>
+					<div class="tech-b3 bg-5" id="tab-5">
+						<h5>Travel</h5>
+						<p>Stay connected whenever you go by plane, train or<br> bus.. Airports, railway stations, bus stations  + planes,<br> trains, buses, etc. You don't need to look for a free<br> power socket anymore! Stay charged with wireless<br> charging stations at your favorite place</p>
+					</div>
+					<div class="tech-b3 bg-6" id="tab-6">
+						<h5>Drones</h5>
+						<p>Drones and quadcopters can be continuously<br> recharged from wireless charging stations<br> on the route. There is an opportunity to create<br> a distributed delivery network with the help <br>of drones, for example, by placing the charging<br> stations on houses</p>
+					</div>
+					<div class="tech-b3 bg-7" id="tab-7">
+						<h5>Industrial</h5>
+						<p>Node technology allows to store and transmit energy wirelessly. Now,<br> you will be fully focused on your production and let us do the work<br> for making all your electrical machinery work for you. No more<br> working or designing around wires, cables, conduit, or outlets. Node<br> delivers wireless power safely and continuously, even in highly<br> volatile environments like chemical manufacture or radiology</p>
+					</div>
+					<div class="block-menu">
+						<ul class="tab-box">
+							<li><a href="#tab-1" class="f-li-a active">Consumer Electronics</a></li>
+							<li><a href="#tab-2">IOT</a></li>
+							<li><a href="#tab-3">Automotive</a></li>
+							<li><a href="#tab-4">Retail</a></li>
+							<li><a href="#tab-5">Travel</a></li>
+							<li><a href="#tab-6">Drones</a></li>
+							<li><a href="#tab-7" class="l-li-a">Industrial</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<section id="tech-b4">
+	<div class="container">
+		<div class="row">
+			<div class="col-xl-12">
+				<h1 class="standart-h1-000">competitive advantages</h1>
+				<div class="tech-b4 mg-t-6">
+					<div class="t-item">
+						<img src="/img/icons/t-ico-1.png" alt="">
+						<p>Transfer energy through<br> any materials</p>
+					</div>
+					<div class="t-item">
+						<img src="/img/icons/t-ico-2.png" alt="">
+						<p>Variety of power<br>solutions</p>
+					</div>
+					<div class="t-item">
+						<img src="/img/icons/t-ico-3.png" alt="">
+						<p>Long distance energy<br> transmission</p>
+					</div>
+					<div class="t-item">
+						<img src="/img/icons/t-ico-4.png" alt="">
+						<p>Any shape, including<br>flexible ones</p>
+					</div>
+					<div class="t-item">
+						<img src="/img/icons/t-ico-5.png" alt="">
+						<p>Energy transmission<br> without a fixed position,<br> in motion as well</p>
+					</div>
+					<div class="t-item">
+						<img src="/img/icons/t-ico-6.png" alt="">
+						<p>Qi standart compatible</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<?php include('footer.php'); ?>
