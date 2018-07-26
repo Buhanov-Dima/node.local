46972221f1c9786dce83b8b6929c95435b1a38f9
Buhanov-Dima <35217770+Buhanov-Dima@users.noreply.github.com>
2018-07-26 15:25:03 +0500

    stellar

--- js/mainpage.js
+++ js/mainpage.js
@@ -1,54 +1,11 @@
 $(document).ready(function(){
 
 var timerId = '0';
-   
-    if ($(window).width()>768) {
+	
 
-        document.onscroll = function() {onWheel();};
-    }
-
-
-    function onWheel(e) {
-        
-        var top = $(window).scrollTop();
-        var yes = $("#block-2").offset().top;
-        
-          if(top > yes/1.4 && $('.news3').hasClass('active')==false && timerId == '0') {
-
-          	timerId = setInterval(function() {
-            
-              if ($('.news-r2').hasClass('active')==true) {
-                $('.news-r2').removeClass('active');
-                $('.news3').addClass('active').animate({opacity: 1}, 600);
-                clearInterval(timerId);
-              }
-
-              if ($('.news2').hasClass('active')==true) {
-                $('.news2').removeClass('active');
-                $('.news-r2').addClass('active').animate({opacity: 1}, 600);
-              }
-
-              if ($('.news-r1').hasClass('active')==true) {
-                $('.news-r1').removeClass('active');
-                $('.news2').addClass('active').animate({opacity: 1}, 600);
-              }
-
-              if ($('.news1').hasClass('active')==true) {
-                $('.news1').removeClass('active');
-                $('.news-r1').addClass('active').animate({opacity: 1}, 600);
-              }
-
-              if ($('.news').hasClass('active')!=true && $('.news-r').hasClass('active')!=true) {
-                $('.news1').addClass('active').animate({opacity: 1}, 600);
-              }
-
-            }, 2500);
-
-          }
-    }
-
-                
- 
-
-
+	$("#par-1").stellar({
+		scrollProperty: 'scroll',
+		parallaxElements: true
+	});
+	
 });
