54dafcc38d72488d5ee86f09e4fb6d738ebe72a3
Buhanov-Dima <35217770+Buhanov-Dima@users.noreply.github.com>
2018-07-27 13:10:54 +0500

    fx

--- js/mainpage.js
+++ js/mainpage.js
@@ -1,31 +1,10 @@
 $(document).ready(function(){
 
-<<<<<<< HEAD
-
-
-
 $('#par-1').paroller();
 $('#par-2').paroller();
 $('#par-3').paroller();
 $('#par-4').paroller();
 $('#par-5').paroller();
 $('#block-2').paroller();
-=======
-
-
-
-	$("#par-1").stellar({
-		scrollProperty: 'scroll',
-		parallaxElements: true
-	});
 	
 });
->>>>>>> 1b97a1f7072092c9f1623bff3d052eb807a2bc43
-
-   
-   
-
-<<<<<<< HEAD
-});
-=======
->>>>>>> 1b97a1f7072092c9f1623bff3d052eb807a2bc43
