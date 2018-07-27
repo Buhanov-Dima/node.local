cecb25a252c2ecd0ba273ca6dfcca735c5fa941d
Buhanov-Dima <35217770+Buhanov-Dima@users.noreply.github.com>
2018-07-27 16:38:47 +0500

    1

--- js/mainpage.js
+++ js/mainpage.js
@@ -8,26 +8,4 @@ $('#par-5').paroller();
 $('.wrapper-plashka').paroller();
 	
 
-  var navbar =  $('.plashka');
-  var wrapper = $('#block-2');
-
-  $(window).scroll(function(){
-    var nsc = $(document).scrollTop();
-    var bp1 = wrapper.offset().top;
-    var bp2 = bp1 + wrapper.outerHeight()+$(window).height();
-    
-    if (nsc>bp1) {  navbar.css('position','fixed'); }
-    else { navbar.css('position','absolute'); }
-    if (nsc>bp2) { navbar.css('top', bp2-nsc); }
-    else { navbar.css('top', '60px'); }
-  });
-
-
-});
-
-$(function() {  
-
-    // Custom Easing
-    
-    
 });
