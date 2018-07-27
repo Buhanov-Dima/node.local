8350894273fc4d37767ce071b9430d9503a04580
DmitriOkulov <dmitriokulov@yandex.ru>
2018-07-27 19:27:25 +0500

    fx

--- js/main.js
+++ js/main.js
@@ -1,8 +1,5 @@
 $(document).ready(function(){
 
-
-    jQuery.scrollSpeed(100, 800);
-
 	$(".careers-ul li").click(function(e) {
 	  e.preventDefault();
 	  $(".careers-ul li").removeClass('active');
--- js/mainpage.js
+++ js/mainpage.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
 
-$('#par-1').paroller();
-$('#par-2').paroller();
-$('#par-3').paroller();
-$('#par-4').paroller();
-$('#par-5').paroller();
-$('.wrapper-plashka').paroller();
-	
+	$('#par-1').paroller();
+	$('#par-2').paroller();
+	$('#par-3').paroller();
+	$('#par-4').paroller();
+	$('#par-5').paroller();
+	$('.wrapper-plashka').paroller();
+	jQuery.scrollSpeed(100, 800);
 
 });
