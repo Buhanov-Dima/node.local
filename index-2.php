77d57eaf9350d4b3938dbd633385b2a71cdeb327
Buhanov-Dima <35217770+Buhanov-Dima@users.noreply.github.com>
2018-07-27 14:26:25 +0500

    1

---
+++ index-2.php
@@ -0,0 +1,129 @@
+<?php include ('header.php'); ?>
+<section id="block-1">
+	<div class="container">
+		<div class="row">
+			<div class="col">
+				<div class="wrap-b1 text-center mg-t-5">
+					<h1>Intelligent Technology</h1>
+					<p class="b1-txt">For Wireless Future</p>
+					<div class="b1-img">
+						<img src="/img/main/shadow-eon.png" alt="">
+					</div>
+				</div>
+				<div class="btn-center">
+					<a href="#" class="btn-down-1"><span></span>More</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<section id="block-2">
+	<div class="container">
+		<div class="row">
+			<div class="col">
+				
+				<div class="block">
+					
+				</div>
+			</div>
+		</div>
+		<div class="row no-gutters">
+			<div class="col-xl-3 col-md-4">
+				<div id="par-1" class="news news1 wrap-b2-txt">
+					<h6>The Future is Wireless</h6>
+					<p>Nodepower is a<br> revolutionary technology,<br> which disrupts a wireless<br> transmission market.</p>
+				</div>
+				<div id="par-2" class="news news2 wrap-b2-txt">
+					<h6>High efficiency</h6>
+					<p>Energy transmission with 95% efficiency.</p>
+				</div>
+				<div id="par-3" class="news news3 wrap-b2-txt">
+					<h6>Long distance</h6>
+					<p>Energy transmission for up to 5 m. (16.4 ft.)</p>
+				</div>
+			</div>
+			<div class="col-xl-6 col-md-4">
+				<div class="plashka">
+					<h1 class="b2-h1">Features</h1>
+					<div class="wrap-b2-img">
+						<img src="/img/main/bg-b2-1.png" alt="">
+					</div>
+				</div>
+			</div>
+			<div class="col-xl-3 col-md-4">
+				<div id="par-4" class="news-r news-r1 wrap-b2-txt">
+					<h6>Multiple devices</h6>
+					<p>Ability to transfer energy<br> to an multiple number of<br> devices in proportion to<br> power consumption.</p>
+				</div>
+				<div id="par-5" class="news-r news-r2 wrap-b2-txt">
+					<h6>Unlimited application</h6>
+					<p>There are no limits for Nodepower technology application. The possibilities of integration range from consumer electronics to industrial use.</p>
+				</div>
+			</div>
+			<div class="col-md-12">
+				<div class="wrap-md-txt">
+					<div class="md-txt-main">
+						<h6>The Future is Wireless</h6>
+						<p>Nodepower is a revolutionary technology, which disrupts a wireless transmission market.</p>
+					</div>
+					<div class="md-txt-main">
+						<h6>High efficiency</h6>
+						<p>Energy transmission with 95% efficiency.</p>
+					</div>
+					<div class="md-txt-main">
+						<h6>Long distance</h6>
+						<p>Energy transmission for up to 5 m. (16.4 ft.)</p>
+					</div>
+					<div class="md-txt-main">
+						<h6>Multiple devices</h6>
+						<p>Ability to transfer energy to an multiple number of devices in proportion to power consumption.</p>
+					</div>
+					<div class="md-txt-main">
+						<h6>Unlimited application</h6>
+						<p>There are no limits for Nodepower technology application. The possibilities of integration range from consumer electronics to industrial use.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!--<div class="btn-center">
+		<a href="#" class="btn-down-1 white-txt"><span></span>Video</a>
+	</div>-->
+</section>
+<section id="block-3">
+	<div class="container">
+		<div class="row">
+			<div class="col">
+				<div class="b3-up">
+					<h1 class="">Technology</h1>
+					<p>Read more about nodepower technology</p>
+					<a href="#" class="btn-down-1 white-txt arrow-right"><span></span>Read more</a>
+				</div>
+				<div class="wrap-b3">
+					<a href="#"><div class="btn-video"></div></a>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<section id="block-4">
+	<div class="node-b4">
+		<div class="logo-node-b4">
+			<img src="/img/icons/logo-wh.png" alt=""><p><b>node</b>power</p>
+		</div>
+		<img src="/img/icons/world-wh.png" alt="">
+	</div>
+	<div class="container">
+		<div class="row">
+			<div class="col">
+				<div class="wrap-b4">
+					<h1 class="standart-h1">Technology</h1>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="btn-center">
+		<a href="#" class="btn-down-1 white-txt arrow-right"><span></span>Read more about nodepower technology</a>
+	</div>
+</section>
+<?php include ('footer.php'); ?>
--- js/mainpage.js
+++ js/mainpage.js
@@ -6,4 +6,20 @@ $('#par-3').paroller();
 $('#par-4').paroller();
 $('#par-5').paroller();
 	
+
+  var navbar =  $('.plashka');
+  var wrapper = $('#block-2');
+
+  $(window).scroll(function(){
+    var nsc = $(document).scrollTop();
+    var bp1 = wrapper.offset().top;
+    var bp2 = bp1 + wrapper.outerHeight()+$(window).height();
+    
+    if (nsc>bp1) {  navbar.css('position','fixed'); }
+    else { navbar.css('position','absolute'); }
+    if (nsc>bp2) { navbar.css('top', bp2-nsc); }
+    else { navbar.css('top', '60px'); }
+  });
+
+
 });
